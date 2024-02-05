62349bfc63d06c0c27531abd39e5b1f086bad304
kunaldhongade <kunaldhongade1@gmail.com>
2024-02-05 17:43:22 +0530

    update: fixed product

--- controller/product.js
+++ controller/product.js
@@ -1,30 +1,4 @@
-// const data = require('../data.json')
-// const products = data.products;
 const { Product } = require("../model/Product");
-
-// MVC model view controller
-// data is model
-// business rule
-
-// view is how we show data to user
-
-// controller is how we control data (model and view changes path)
-// all logic in controller
-
-// exports.createUser = async (req, res) => { // this is controller logically
-//     // const product = req.body
-//     // console.log(product)
-//     // products.push(product)
-
-//     const product = new Product(req.body);
-//     // product.title = "Phone X";
-//     // product.price = 9999;
-//     // product.description = "This is a good phone";
-//     // product.rating = 4.5;
-//     // // we do not declare img but its initialize because arrays default value is empty array
-//     await product.save()
-//     res.status(201).json(product)
-// }
 
 exports.createUser = async (req, res) => {
   try {
@@ -35,8 +9,6 @@ exports.createUser = async (req, res) => {
     if (error.code === 11000) {
       console.error("Duplicate key error. Document already exists!");
       res.status(400).json(error);
-
-      // Handle the duplicate key error here (e.g., retry with different data)
     } else {
       console.error("An error occurred:", error);
       res.status(400).json(error);
@@ -54,10 +26,6 @@ exports.getAllProducts = async (req, res) => {
 };
 
 exports.getProduct = async (req, res) => {
-  // const id = +req.params.id
-  // const product = products.find(product => product.id === id)
-  // res.json(product)
-
   try {
     const id = req.params.id;
     const product = await Product.findById(id);
@@ -68,12 +36,6 @@ exports.getProduct = async (req, res) => {
 };
 
 exports.replaceProduct = async (req, res) => {
-  // const id = +req.params.id
-  // const productIndex = products.findIndex(product => product.id === id)
-  // products.splice(productIndex, 1, { ...req.body, id })
-  // // change to which index, what to delete, new obj to replace
-  // res.status(200).json({ 1: "updated" });
-
   try {
     const id = req.params.id;
     const doc = await Product.findOneAndReplace({ _id: id }, req.body, {
@@ -86,14 +48,6 @@ exports.replaceProduct = async (req, res) => {
 };
 
 exports.updateProduct = async (req, res) => {
-  // const id = +req.params.id
-  // const productIndex = products.findIndex(product => product.id === id)
-  // const product = products[productIndex]
-  // products.splice(productIndex, 1, { ...product, ...req.body })
-  // // change to which index, number of elements to delete, this will replace property of product by req.body only mentioned get replaced
-
-  // res.status(200).json({ 1: "updated" });
-
   try {
     const id = req.params.id;
     const doc = await Product.findOneAndUpdate({ _id: id }, req.body, {
@@ -106,12 +60,6 @@ exports.updateProduct = async (req, res) => {
 };
 
 exports.deleteProduct = async (req, res) => {
-  // const id = +req.params.id
-  // const index = products.findIndex(product => product.id === id)
-  // const product = products[index]
-  // products.splice(index, 1)
-  // res.status(204).json(product)
-
   try {
     const id = req.params.id;
     const doc = await Product.findByIdAndDelete(id);
