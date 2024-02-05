6d9ce90561c9739d23a0870555bfd06355239dd3
kunaldhongade <kunaldhongade1@gmail.com>
2024-02-05 17:43:35 +0530

    update: fixed product

--- model/Product.js
+++ model/Product.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-// schema is configuration of collection
 const productSchema = new Schema({
   title: {
     type: String,
     required: true,
     unique: true,
-  }, // String is shorthand for {type: String}
+  },
   description: { type: String },
   price: {
     type: Number,
@@ -33,15 +32,4 @@ const productSchema = new Schema({
   images: [String],
 });
 
-// model is collection. Schema is configuration to create model class
 exports.Product = mongoose.model("Product", productSchema);
-// model name is Product and collection name will be products
-//model things are always singular
-// model combines schema and collection
-// we can create many models for different collections in same db or different db also but we need to connect to db first before creating model or schema or collection or document
-
-// document is instance of model class
-// document is row in collection
-// document things are always plural
-// we can perform CRUD operations on document using model (Product)
-// we can create document using model
